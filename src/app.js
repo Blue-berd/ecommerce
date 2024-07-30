9ddf1880321c5281d879e5d58e95ddc55b389870
jay-ekodus <jsardar@ekodusinc.com>
2024-07-31 00:42:52 +0530

    added

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use((req, res, next) => {
-  console.log(req.headers.authorization);
+  console.log("body", req.headers["Authorization"]);
   next();
 });
 app.use("/api/auth", authRoutes);
